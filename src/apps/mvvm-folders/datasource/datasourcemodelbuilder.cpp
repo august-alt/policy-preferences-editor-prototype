9e645275749148a58ff08b18f410b98b939f6e9e
august-alt <77973983+august-alt@users.noreply.github.com>
2022-05-25 17:20:41 +0400

    feat: add data source model builder

---
+++ src/apps/mvvm-folders/datasource/datasourcemodelbuilder.cpp
@@ -0,0 +1,75 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2021 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#include "datasourcemodelbuilder.h"
+
+#include "datasourcecontaineritem.h"
+#include "datasourceitem.h"
+
+#include "common/commonitem.h"
+
+namespace mvvm_folders
+{
+
+std::unique_ptr<PreferencesModel> ModelBuilder::schemaToModel(std::unique_ptr<DataSources> &dataSources)
+{
+    auto model = std::make_unique<PreferencesModel>();
+
+    for (const auto& dataSourceSchema : dataSources->DataSource())
+    {
+        auto properties = dataSourceSchema.Properties();
+        auto dataSource = DataSourceItem();
+        dataSource.setProperty(DataSourceItem::ACTION, properties.action()->c_str());
+        dataSource.setProperty(DataSourceItem::USERDSN, properties.userDSN().get());
+        dataSource.setProperty(DataSourceItem::DSN, properties.dsn().c_str());
+        dataSource.setProperty(DataSourceItem::DRIVER, properties.driver().c_str());
+        dataSource.setProperty(DataSourceItem::DESCRIPTION, properties.description()->c_str());
+        dataSource.setProperty(DataSourceItem::USERNAME, properties.username()->c_str());
+        dataSource.setProperty(DataSourceItem::CPASSWORD, properties.cpassword()->c_str());
+        dataSource.setProperty(DataSourceItem::USERSOURCE, properties.userDSN() ? true : false);
+        dataSource.setProperty(DataSourceItem::SYSTEMSOURCE, properties.userDSN() ? false : true);
+
+        // TODO: Implement attributes and correct schema.
+
+        auto common = CommonItem();
+        common.setProperty(CommonItem::NAME, dataSourceSchema.name().c_str());
+        common.setProperty(CommonItem::CHANGED, dataSourceSchema.changed()->c_str());
+        common.setProperty(CommonItem::DESC, dataSourceSchema.desc()->c_str());
+        common.setProperty(CommonItem::BYPASS_ERRORS, dataSourceSchema.bypassErrors().get());
+        common.setProperty(CommonItem::USER_CONTEXT, dataSourceSchema.userContext().get());
+        common.setProperty(CommonItem::REMOVE_POLICY, dataSourceSchema.removePolicy().get());
+
+        auto sessionItem = model->insertItem<DataSourceContainerItem>(model->rootItem());
+        sessionItem->setDataSource(dataSource);
+        sessionItem->setCommon(common);
+    }
+
+    return model;
+}
+
+std::unique_ptr<DataSources> ModelBuilder::modelToSchema(std::unique_ptr<PreferencesModel> &model)
+{
+    Q_UNUSED(model);
+
+    return nullptr;
+}
+
+}
+
